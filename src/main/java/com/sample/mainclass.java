ef37cd0bcdcdc8f18ebe7603a27f91e0dc92058a
nizarm78 <nzr.a.mhd@gmail.com>
2015-03-19 22:45:02 +0200

    adding comments for documentation on project main class

--- src/main/java/com/sample/mainclass.java
+++ src/main/java/com/sample/mainclass.java
@@ -16,19 +16,27 @@ public class mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// initialize knowledge base
 		KieHelper khelper = new KieHelper();
+		
+		// adding resource to knowledge base
 		khelper.addResource(ResourceFactory.newClassPathResource("sample.bpmn"), ResourceType.BPMN2);
 		khelper.addResource(ResourceFactory.newClassPathResource("ruletest.drl"), ResourceType.DRL);
-
+		
+		// building the default knowledge base
 		KieBase kbase = khelper.build();
 		
+		// using the knowledge base to build the session base
 		KieSession ksessionBase= kbase.newKieSession();
 		
+		// initializing process variables
 		Map params = new HashMap<String, Subscriber>();
 		params.put("subscriber", new Subscriber("nizar", 4));
 		
+		// starting the process using knowledge base, passing the process variables
 		ksessionBase.startProcess("com.sample.bpmn.hello", params);
 		
+		// fire all rules
 		ksessionBase.fireAllRules();
 	}
 
